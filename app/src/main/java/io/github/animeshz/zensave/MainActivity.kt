1c6fabd568284220cceefebb1dc273f4455f8e8f
Animesh Sahu <animeshsahu19@yahoo.com>
2024-07-15 10:34:19 +0530

    feat: add splash screen to the app

--- app/src/main/java/io/github/animeshz/zensave/MainActivity.kt
+++ app/src/main/java/io/github/animeshz/zensave/MainActivity.kt
@@ -5,27 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.animeshz.zensave.ui.theme.ZenSaveTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            ZenSaveTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    ZenSaveTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            innerPadding
         }
     }
 }
@@ -36,12 +42,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZenSaveTheme {
-        Greeting("Android")
-    }
 }
